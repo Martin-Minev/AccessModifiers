b6030123286f07406a778336c6bb42ebe323279b
Martin-Minev <72812000+Martin-Minev@users.noreply.github.com>
2021-02-28 22:32:02 +0100

    1st

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Base baseObject = new Base();
+            baseObject.BaseMathod();
+
+            Child child = new Child();
+            child.BaseMathod();
+        }
+    }
+}
